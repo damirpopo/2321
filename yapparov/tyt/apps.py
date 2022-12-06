8098ad26dc5a7554db032202d01acfa0c7e58a2a
damirpopo <43522476+damirpopo@users.noreply.github.com>
2022-12-07 00:41:46 +0300

    Add files via upload

---
+++ yapparov/tyt/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TytConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tyt'
---
+++ yapparov/yapparov/urls.py
@@ -0,0 +1,6 @@
+from tyt import views
+from django.urls import path
+
+urlpatterns = [
+    path('',views.index),
+]
